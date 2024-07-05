6684766def375aefb3b626b683a0cc7917a50c1e
vasathotaarun <vasathotaarun00@gmail.com>
2024-07-05 12:12:13 +0530

    Updated Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -1,14 +1,27 @@
 import unittest
 from Soundex import generate_soundex
-
+from Soundex import encode_name
+ 
 class TestSoundex(unittest.TestCase):
-
+ 
     def test_empty_string(self):
         self.assertEqual(generate_soundex(""), "")
-
+ 
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
-
+ 
+    def test_encode_name(self):
+        self.assertEqual(encode_name("Aeiou","A",0),"A000")
+ 
+    def test_long_string(self):
+        self.assertEqual(generate_soundex("Jackandsmith"), "J532")      
+ 
+    def test_non_alphabetic_characters(self):
+         self.assertEqual(generate_soundex("1234"), "1000")
+ 
+    def test_mixed_chars(self):
+        self.assertEqual(generate_soundex("Jackfruit@home"), "J163")
+ 
     
 if __name__ == '__main__':
     unittest.main()
